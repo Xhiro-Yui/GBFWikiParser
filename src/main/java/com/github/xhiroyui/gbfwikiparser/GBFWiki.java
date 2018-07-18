6f6ebba6a8bbc0fd961918a0565de5a42b8fe1e9
Xhiro Yui <xhiro.yui@gmail.com>
2018-07-18 18:58:49 +0800

    V1.0.1

--- src/main/java/com/github/xhiroyui/gbfwikiparser/GBFWiki.java
+++ src/main/java/com/github/xhiroyui/gbfwikiparser/GBFWiki.java
@@ -122,7 +122,7 @@ public class GBFWiki {
          * @return A list containing objects of the specified type. List will be empty if no results are found.
          */
         public List<GBFWikiObject> processData() {
-            log.info("Parsing data from GBFWiki with {} {} {} parameters {} {} {}.", "1", 2, "more", 12312);
+//            log.info("Parsing data from GBFWiki with {} {} {} parameters {} {} {}.", "1", 2, "more", 12312);
             List<GBFWikiObject> data = new ArrayList<>();
             if (results.objectType == WikiObjectType.WEAPON) {
                 try (InputStreamReader reader = new InputStreamReader(new URL(MessageFormat.format("{0}&tables=weapons&fields=name,title,grp=group,rarity,element,type,jpname=jpName,jptitle=jpTitle,release_date=releaseDate,obtain,obtain_text=obtainText,parent,img_full=imgFull,img_icon=imgIcon,img_square=imgSquare,img_tall=imgTall,evo_min=evoMin,evo_base=evoBase,evo_max=evoMax,evo_red=evoRed,atk1,atk2,atk3,atk4,hp1,hp2,hp3,hp4,ca1_name=ougi1Name,ca1_desc=ougi1Desc,ca2_name=ougi2Name,ca2_desc=ougi2Desc,s1_icon=skill1Icon,s1_name=skill1Name,s1_lvl=skill1Level,s1_desc=skill1Desc,s1u1_icon=skill1Upgrade1Icon,s1u1_name=skill1Upgrade1Name,s1u1_lvl=skill1Upgrade1Level,s1u1_desc=skill1Upgrade1Desc,s2_icon=skill2Icon,s2_name=skill2Name,s2_lvl=skill2Level,s2_desc=skill2Desc,s2u1_icon=skill2Upgrade1Icon,s2u1_name=skill2Upgrade1Name,s2u1_lvl=skill2Upgrade1Level,s2u1_desc=skill2Upgrade1Desc,bullets,bullet1,bullet2,bullet3,bullet4,bullet5,bullet6,bullet7,bullet8,bullet9&limit={1}&where=name%20LIKE%20%22%{2}%%22", Constants.GBF_WIKI_LINK_SUFFIX, results.queryLimit, results.query)).openStream())) {
@@ -175,8 +175,6 @@ public class GBFWiki {
 
         }
     }
-
-
 
 
 }
